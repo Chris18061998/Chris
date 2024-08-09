1f792c3cf452cbcf270c17e9f3aa069595b5d470
Chris <jisporraito@gmail.com>
2024-08-09 20:00:17 +0530

    Adding New Feature

---
+++ Mainpgm.py
@@ -0,0 +1,4 @@
+def Mainpgm():
+	print("Hi I am inside the Mainpgm")
+def Maintwo():
+	print("Hi I am the second pgm in Main")
---
+++ Sub/Subpgm.py
@@ -0,0 +1,4 @@
+def Submain():
+	print("Hi I am inside the sub pgm")
+def Subtwo():
+	print("Hi I am inside 2nd pgm in sub")
